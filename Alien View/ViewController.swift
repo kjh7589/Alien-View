d50a0505c8c5b0297161105095120b3edd18cbc7
D7703_30 <d7703_30@d7703-30ui-imac.local>
2018-04-03 15:36:52 +0900

    Initial Commit

---
+++ Alien View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Alien View
+//
+//  Created by D7703_30 on 2018. 4. 3..
+//  Copyright © 2018년 김재현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
